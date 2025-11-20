560128bee6cac21d054afee77b3cd3eb81b2636b
Leny Garcia <lenygarcia0302@gmail.com>
2025-11-20 11:48:31 +0100

    fix(predict.py): fix indentation for main function, fix indentation for empty docstring fix(train.py): fix indentation for estimated_price function, fix indentation for train function, fix indentation for main function

--- train.py
+++ train.py
@@ -1,59 +1,66 @@
 import csv
+import matplotlib.pyplot as plt
 
 
 def estimated_price(mileage, theta0, theta1):
-	""""""
-	return theta0 + theta1 * mileage
+    """"""
+    return theta0 + theta1 * mileage
 
 
 def train(path: str, learning_rate=5e-12, iterations=10000):
-	""""""
-	x = []
-	y = []
+    """"""
+    x = []
+    y = []
 
-	with open(path, "r") as f:
-		data = csv.reader(f)
-		next(data)
-		for row in data:
-			x.append(float(row[0]))
-			y.append(float(row[1]))
-	
-	m = len(x)
-	theta0 = 0.0
-	theta1 = 0.0
+    with open(path, "r") as f:
+        data = csv.reader(f)
+        next(data)
+        for row in data:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
 
-	for _ in range(iterations):
-		sum_error_theta0 = 0.0
-		sum_error_theta1 = 0.0
+    m = len(x)
+    theta0 = 0.0
+    theta1 = 0.0
 
-		for i in range(m):
-			pred = estimated_price(x[i], theta0, theta1)
-			error = pred - y[i]
+    for _ in range(iterations):
+        sum_error_theta0 = 0.0
+        sum_error_theta1 = 0.0
 
-			sum_error_theta0 += error
-			sum_error_theta1 += error * x[i]
+        for i in range(m):
+            pred = estimated_price(x[i], theta0, theta1)
+            error = pred - y[i]
 
-		tmp0 = theta0 - learning_rate * (1 / m) * sum_error_theta0
-		tmp1 = theta1 - learning_rate * (1 / m) * sum_error_theta1
+            sum_error_theta0 += error
+            sum_error_theta1 += error * x[i]
 
-		theta0 = tmp0
-		theta1 = tmp1
-	
-	with open("theta.csv", "w") as f:
-		f.write(f"{theta0},{theta1}")
-	
-	print("TRAINING DONE")
-	print(f"Theta0 = {theta0}")
-	print(f"Theta1 = {theta1}")
+        tmp0 = theta0 - learning_rate * (1 / m) * sum_error_theta0
+        tmp1 = theta1 - learning_rate * (1 / m) * sum_error_theta1
+
+        theta0 = tmp0
+        theta1 = tmp1
+
+    with open("theta.csv", "w") as f:
+        f.write(f"{theta0},{theta1}")
+
+    print("TRAINING DONE")
+    print(f"Theta0 = {theta0}")
+    print(f"Theta1 = {theta1}")
+    # Plotting the data and the regression line
+    plt.scatter(x, y)
+    plt.title("Mileage vs Price")
+    plt.xlabel("Mileage")
+    plt.ylabel("Price")
+    plt.show()
 
 
 def main():
-	""""""
-	try:
-		train("data.csv")
-	except Exception as e:
-		print(f"An error as occured : {e}")
+    """"""
+    try:
+        train("data.csv")
+    except Exception as e:
+        print(f"An error as occured : {e}")
 
 
 if __name__ == "__main__":
-	main()
+    main()
